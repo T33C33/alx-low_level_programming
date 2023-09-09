da806e2e077b972ff576d6eb5f2b1766a42cb811
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-08 20:02:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - main function to print a text accurately
+ * Return: returns (0)
+ *
+ * Description: prints exactly a string of texts
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
