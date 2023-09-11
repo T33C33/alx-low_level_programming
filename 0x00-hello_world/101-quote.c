fa26f1edbc761b7de9b600810847c87b87bdd094
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-10 17:50:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /**
  * main - prints a string of texts without printf or puts
  * Return: returns 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59\n");
+	write(2"and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59\n", 59);
 	return (1);
 }
