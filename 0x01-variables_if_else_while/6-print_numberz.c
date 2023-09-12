d1c819f21c79c5f994d1b11482ac30ccfa142959
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-11 20:19:56 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Description: prints base 10 numbers of starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
