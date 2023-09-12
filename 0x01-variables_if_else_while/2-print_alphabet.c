d9a3e3596b9665201a4ccbb2a580f6d61fe8a2aa
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-11 19:18:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * @c: char variable
+ *
+ * Description: program that prints the alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lc = 'a';
+
+	while (lc <= 'z')
+	{
+		putchar(lc);
+		lc++;
+	}
+	putchar('\n');
+	return (0);
+}
