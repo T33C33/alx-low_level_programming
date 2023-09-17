715b5117bca6f4313c599d21323155509bddad1c
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-17 16:12:38 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: formal parameter
+ *
+ * Description: prints the n times table, starting with 0
+ * Return: No return type, hence no return value
+ */
+void print_times_table(int n)
+{
+	int x = 0;
+
+	if ((n >= 0) && (n <= 15))
+	{
+		for (x = 0; x <= n; x++)
+		{
+			int y = 0;
+
+			for  (y = 0; y <= n; y++)
+			{
+				int sum = x * y;
+
+				if ((y != 0) && (sum < 10))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(sum + '0');
+				}
+				else if ((sum >= 10) && (sum < 100))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((sum / 10) + '0');
+					_putchar((sum % 10) + '0');
+				}
+				else if (sum >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((sum / 100) + '0');
+					_putchar(((sum / 10) % 10) + '0');
+					_putchar((sum % 10) + '0');
+				}
+				else if (y == 0)
+				{
+					_putchar(sum + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
