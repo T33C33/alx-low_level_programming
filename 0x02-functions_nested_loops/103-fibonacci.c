ff830cc55422970e1ba06ec0cce3c5f41b5d2dad
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-17 17:38:22 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the even-valued terms
+ *
+ * Description: prints the sum of the even-valued terms
+ * Return: Always returs 0
+ */
+int main(void)
+{
+	int x = 1;
+	int sum = 0;
+
+	for (x = 1; x <= 4000000; x++)
+	{
+		if ((x % 10) == 0)
+		{
+			sum = sum + x;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
