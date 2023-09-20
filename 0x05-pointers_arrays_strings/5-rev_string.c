43c41767ca782893adf1f6259d4e375587d27291
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-19 18:22:01 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,8 +17,8 @@ void rev_string(char *s)
 	}
 	for (i = 0; i < length / 2; i++)
 	{
-	       sto = s[i];
-	       s[i] = s[length - 1 - i];
-	       s[length - i - 1] = sto;
+		sto = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - i - 1] = sto;
 	}
 }
