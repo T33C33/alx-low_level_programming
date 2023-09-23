63be0b565ca7bcbc787c7c1d771c5313687243b9
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-23 15:46:31 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,35 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
- * @s: pointer to a char (formal parameter)
+ * @s: pointer to char (formal parameter)
  *
  * Description: number can be preceded by an infinite number of characters
- * take into account all the - and + signs before the number
- * Return: returns 0 if no numbers exist in the string
+ * taking into account + and - signs
+ * not allowed to use long
+ * Return: 0 if no numbers exist
  */
 int _atoi(char *s)
 {
-	int start = 0, num = 0, i = 0, final = 0, multi = 1;
+    int i = 0;
+    int final = 0;
+    int min_add = 1;
 
-	while (s[i] != '\0')
-	{
-		if ((s[i] >= '0') && s[i] <= '9')
-		{
-			start = 1;
-			num = s[i] - '0';
-			final = final * 10 + num;
-		}
-		else
-		{
-			if (start)
-			{
-				break;
-			}
-			else if (s[i] == '-')
-			{
-				multi = -1;
-				start = 1;
-			}
-			else if (s[i] == '+')
-			{
-				start = 1;
-			}
-			else if (s[i] != ' ' && !start)
-			{
-				break;
-			}
-		}
-		i++;
-	}
-	return (multi * final);
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+        {
+            min_add *= -1;
+        }
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            final = final * 10 + (s[i] - '0');
+        }
+        else if (final > 0)
+        {
+            break;
+        }
+        i++;
+    }
+    return final * min_add;
 }
