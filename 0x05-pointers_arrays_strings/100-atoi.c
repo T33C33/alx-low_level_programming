4a68c8fea4b23403ed14d4563361ba3f69f50fe7
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-23 15:55:30 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,25 +11,25 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int final = 0;
-    int min_add = 1;
-
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        {
-            min_add *= -1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            final = final * 10 + (s[i] - '0');
-        }
-        else if (final > 0)
-        {
-            break;
-        }
-        i++;
-    }
-    return final * min_add;
+	int i = 0;
+	int final = 0;
+	int min_add = 1;
+	
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			min_add *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			final = final * 10 + (s[i] - '0');
+		}
+		else if (final > 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return final * min_add;
 }
