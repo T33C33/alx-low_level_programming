31e890b2b562f02b4271931eee1130ffc5410612
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-24 20:32:31 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @cap: formal parameter pointer to a char
+ *
+ * Description: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }"
+ * Return: pointer to char cap
+ */
+int sep_func(char c)
+{
+	char sep[] = " \t\n,;.!?\"(){}";
+	int i;
+	
+	for (i = 0; sep[i] != '\0'; i++)
+	{
+		if (c == sep[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+char *cap_string(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0 || sep_func(str[i - 1]))
+			{
+				str[i] -= 32;
+			}
+		}
+	}
+	return (str);
+}
