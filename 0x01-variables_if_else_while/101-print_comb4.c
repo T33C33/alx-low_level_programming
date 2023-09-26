9b3613dfb718c6ebc183ca6ff37fa0ef1ec61f74
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-26 06:09:33 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,34 +8,29 @@
  */
 int main(void)
 {
-	int x = 0;
-	int y = 1;
-	int z = 2;
+	int x = 0, y, z;
 
-	while (x <= 7)
+	while (x <= 9)
 	{
-		while (y <= 8)
+		y = x + 1;
+		while (y <= 9)
 		{
+			z = y + 1;
 			while (z <= 9)
 			{
-				if ((x < y) && (y < z))
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				if (x != 7 || y != 8 || z != 9)
 				{
-					putchar(x + '0');
-					putchar(y + '0');
-					putchar(z + '0');
-					if ((x != 7) || (y != 8) || (z != 9))
-					{
-						putchar(',');
-						putchar(' ' );
-					}
+					putchar(',');
+					putchar(' ');
 				}
 				z++;
 			}
 			y++;
 		}
 		x++;
-		y = x + 1;
-		z = y + 1;
 	}
 	putchar('\n');
 	return (0);
