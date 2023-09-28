eecfdd7a339112bedfff395cbc82651d87585a73
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-28 06:45:18 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,14 +14,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		if (n <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\n');
-		}
-		else
 			_putchar('-');
+		}
 	}
 	_putchar('\n');
 }
