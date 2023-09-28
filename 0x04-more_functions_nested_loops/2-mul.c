d56f5a5b86333b796088a5e3a64081fba5d89867
T33C33 <teeceeiheukwumere@gmail.com>
2023-09-28 01:42:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: 1st formal parameter
+ * @b: 2nd formal parameter
+ *
+ * Description: a function that multiplies two integers
+ * Return: Always returns product of  2 digits
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
