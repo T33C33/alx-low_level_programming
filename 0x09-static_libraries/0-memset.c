8f74a14295472250956febe5c4c8d7eb2737e299
T33C33 <teeceeiheukwumere@gmail.com>
2023-10-03 18:06:31 -0700

    static library whahala

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - empty function
+ * @s: formal paramter
+ * @b: formal parameter
+ * @n: formal parameter 3
+ *
+ * Return: a value of type int
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first formal parameter
+ * @src: second formal parameter to append
+ *
+ * Description: function appends the src string to the dest string
+ * overwriting the terminating null byte (\0) at the end of dest
+ * and then adds a terminating null byte
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *concat = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (concat);
+}
