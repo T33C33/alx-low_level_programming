930268847d73bc3b8bdd30e0e45596e73c8e8f36
T33C33 <teeceeiheukwumere@gmail.com>
2023-11-15 20:06:04 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,9 +2,9 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: formal parameter pointer to a char
+ * @s: formal parameter pointer to char
  *
- * Description: a function that prints a string in reverse
+ * Description: function that prints string in reverse
  * Return: Void
  */
 void _print_rev_recursion(char *s)
