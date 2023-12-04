a9b0ee067e6a1c8094187a14f9e618051c81d7ef
T33C33 <teeceeiheukwumere@gmail.com>
2023-12-03 16:28:50 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Declaring dog's name, age and owner
  * @name: First member
@@ -10,3 +13,5 @@ struct dog
 	float age;
 	char *owner;
 }
+
+#endif
