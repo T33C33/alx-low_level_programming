a3cb29e6362ab706d079304342858360d1b0a7e6
T33C33 <teeceeiheukwumere@gmail.com>
2023-12-03 17:32:38 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,10 @@ void print_dog(struct dog *d)
 	{
 		if (((*d).name == NULL) || ((*d).owner == NULL))
 		{
-			printf("Name: (nil)\nAge: (nil)\nOwner: (nil)\n");
+			printf("Name: %s\nAge: %f\nOwner: %s\n",
+					(d->name != NULL) ? d->name : "(nil)",
+					(d->name != NULL) ? d->age : 0.0,
+					(d->owner != NULL) ? d->owner : "(nil)");
 		}
 		else
 		{
