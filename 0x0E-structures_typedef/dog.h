3add283c6787ad8a42f3fdaa3cde89839d34b1c8
T33C33 <teeceeiheukwumere@gmail.com>
2023-12-03 16:21:24 -0800

    testing identifier error

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - Declaring dog's name, age and owner
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
