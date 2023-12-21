7103157581aadc422e061308d0bb0466cc16adef
T33C33 <teeceeiheukwumere@gmail.com>
2023-12-21 04:22:20 -0800

    error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file = fopen(filename, "w");
-	size_t write_count = 0;
+	FILE *file;
+	size_t c = 0;
+	int fd = 0;
 
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	if (file == NULL)
+	file = fopen(filename, "w");
+	if ((filename == NULL) || (file == NULL))
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		write_count = fwrite(text_content, sizeof(char), strlen(text_content), file);
-	}
-	if (write_count != strlen(text_content))
-	{
-		fclose(file);
-		return (-1);
+		c = fwrite(text_content, sizeof(char), strlen(text_content), file);
 	}
 	fclose(file);
+	if (c != strlen(text_content))
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	close(fd);
 	return (1);
 }
