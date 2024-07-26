c6853e5f476a167e55169dc3fb68e9e0f8a9f89d
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-26 17:19:28 +0100

    new

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,21 +7,24 @@
  * @array: input array
  * @size: size of the array
  * @value: value to search for
- * Return: Always EXIT_SUCCESS
+ * Return: Index of the value if found, -1 otherwise
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
