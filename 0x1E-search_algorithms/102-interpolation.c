2f08bfbf2791e83386314b79cdba08d760039e9c
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-28 12:57:40 +0100

    new

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,7 +12,7 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1;
+	size_t left = 0, right = size - 1, index;
 
 	if (array == NULL)
 		return (-1);
@@ -29,7 +29,7 @@ int interpolation_search(int *array, size_t size, int value)
 			break;
 		}
 
-		size_t index = left + (((double)(right - left)
+		index = left + (((double)(right - left)
 				/ (array[right] - array[left])) * (value - array[left]));
 
 		if (index >= size)
