1ff57923ec2233ec1dece24121f1127903db1bc9
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-10 03:40:51 +0100

    new

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,10 +15,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (current != NULL)
 	{
 		if (count == index)
-			return current;
+			return (current);
 		count++;
 		current = current->next;
 	}
 
-	return NULL;
+	return (NULL);
 }
